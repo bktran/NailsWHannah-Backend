5e740aaecf307662bc8d1385b305daa3ef722a4e
unknown <BaoKhanh@DKvXiA1pV.DKvXiA1pV>
2024-01-21 15:39:21 -0800

    degbug

--- index.js
+++ index.js
@@ -7,41 +7,65 @@ const app = express() //firing up express/server
 
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, 
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 })
 
 // REQUEST - get info from frontend/send to backend
 // RESPONSE - send to frontend
 app.get('/api/get', (request, response) => {
-  const sql = 
-  `
+  const sql = `
     SELECT * FROM reviews;
   `
   db.query(sql, (err, result) => {
-    response.send(result) //Send to Frontend
-    console.log("🚀 ~ db.query ~ result:", result)
+    response.send(result) //Send to Frontend (client)
   })
 
 })
 
 app.post('/api/insert', (request, response) => {
-  const reviewer = request.body.reviewer
-  const reviewText = request.body.reviewText
-  const sql =
-  `
+  const client_name = request.body.client_name //use body because we are passing in the data
+  const review_text = request.body.review_text
+
+  const sql = `
     INSERT INTO reviews (client_name, review) 
     VALUES (?, ?);
   `
-  db.query(sql, [reviewer, reviewText], (err, result) => {
+  db.query(sql, [ client_name, review_text ], (err, result) => {
     console.log(err)
   })
 
+})
+
+app.delete("/api/delete/:id", (req, res) => {
+  const id = req.params.id //use params because we are passing in the id
+  const sql = `
+    DELETE FROM reviews WHERE id = ?
+  `
+  db.query(sql, id, (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+})
+
+app.put("/api/update/:id", (req, res) => {
+  const id = req.params.id //use params because we are passing in the id
+  const review_text = req.body.review_text
+  const sql = `
+    UPDATE reviews SET review =? WHERE id =?
+  `
+  // set array in order of ? ?
+  db.query(sql, [ review_text, id ], (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+  })
 })
 
 //RUNNING ON LOCALHOST:3001
